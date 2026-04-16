08ccdd627d5c45c5f044d8e7a1c3e01d0d048bfa
naaiyy <naaiyyyy@gmail.com>
2026-04-16 03:42:19 +0200

    fix spacing

--- apps/web/modules/editor/site-editor.tsx
+++ apps/web/modules/editor/site-editor.tsx
@@ -259,7 +259,7 @@ function SiteEditorInner({
 
           <PortalContainerProvider value={portalContainer}>
             <div
-              className="overflow-visible p-4 pb-24"
+              className="overflow-visible p-4 pb-20"
               style={customizationStyles}
               {...(isCustomized ? { "data-site-customized": "" } : {})}
             >
